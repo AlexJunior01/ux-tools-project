726147e109d95640b87ed27bec42c887311b0478
Alex Junior <alexmomijunior@gmail.com>
2022-07-31 20:25:49 -0300

    Search tool functionando adição e remoção

--- src/script.js
+++ src/script.js
@@ -1,97 +1,180 @@
-//import MiniSearch from 'minisearch';
-
+// Lista de padrões para simular o banco e dados
 const patterns = 
     [
-     {"title": "identify real points of UX improvement",
+     {"id": 0,
+      "title": "identify real points of UX improvement",
      "description": "In the stabilization stage, startups seek to rapidly evolve their business sustainably. In this stage, the focus is to develop products and services that are easy to maintain and create an efficient infrastructure for user support and appoint professionals qualified (skilled) in customer experience (or customer success)."},
      
-     {"title": "bring together user information",
+     {"id": 1,
+     "title": "bring together user information",
      "description": "Startups operate in a very dynamic market. To evolve rapidly and improve their product and services, the startups usually collect user feedback through different touch-points, e.g., marketing, support, and sales."},
      
-     {"title": "document demands coming from users ",
+     {"id": 2,
+     "title": "document demands coming from users ",
      "description": "Startups are able to quickly react to changes to fit their products and services to market demands. Something that can take months for traditional companies to change, startups usually have little time to monitor and understand the behavior of their users to make decisions."},
      
-     {"title": "identify [UX] problems/improvements through metrics",
+     {"id": 3,
+     "title": "identify [UX] problems/improvements through metrics",
      "description": "Software startups typically operate in uncertain contexts and under a lack of resources. Metrics can help startups optimize these few resources by making the right decisions at the right time. "},
      
-     {"title": "identify UX insights from user data",
+     {"id": 4,
+     "title": "identify UX insights from user data",
      "description": "Startups struggle to rapidly evolve a product to market, and then scale their activities. Startups operate in a reactive manner and gathering feedback from customers and users is the main way to adjust the direction of their business."},
      
-     {"title": "responding to users' demands ",
+     {"id": 5,
+     "title": "responding to users' demands ",
      "description": "Many software startups recognize the importance of having a customer-centric business model. The customer-centric mindset implies listening to the customers/users, seeking to offer positive experiences through interaction with your products and services."},
      
-     {"title": "conduct product research/evaluations with real users",
+     {"id": 6,
+     "title": "conduct product research/evaluations with real users",
      "description": "Many startup entrepreneurs are well aware of the importance of a good user experience to the success of their products and services. "},
      
-     {"title": "document UX artifacts and decisions",
+     {"id": 7,
+     "title": "document UX artifacts and decisions",
      "description": "Operating in uncertain markets and with limited resources, startups need more flexible work practices rather than more structured and rigid processes to evolve quickly."},
      
-     {"title": "promote UX culture",
+     {"id": 8,
+     "title": "promote UX culture",
      "description": "Few startups are able to embrace UX work from day one. In the beginning, the team is small and the professionals work on several fronts, accumulating roles to achieve a functional and a more stable version of the product."},
      
-     {"title": "understand the value of UX ",
+     {"id": 9,
+     "title": "understand the value of UX ",
      "description": "Software startups operate within the highly competitive market, focusing on innovative and disruptive ideas with value propositions that meet the desires of their customers. In this way, startups face many uncertainties in the market, regarding product features and resources."},
      
-     {"title": "improve communication between teams",
+     {"id": 10,
+     "title": "improve communication between teams",
      "description": "Time pressure and lack of resources lead startups to adopt more flexible managerial and organizational practices."},
      
-     {"title": "how to start doing UX",
+     {"id": 11,
+     "title": "how to start doing UX",
      "description": "In the beginning, startups focus on validating and developing products and services as soon as possible towards gaining a customer base rapidly."},
      
-     {"title": "professional input on UX",
+     {"id": 12,
+     "title": "professional input on UX",
      "description": "Startups have lots of business details to manage and prioritize in the early stages."},
      
-     {"title": "define roles and jobs description for UX ",
+     {"id": 13,
+     "title": "define roles and jobs description for UX ",
       "description": "Only a few startups have financial resources and the desire to invest in UX design activities before the company begins to grow and attract users in fact. Startups seek to evolve quickly by adopting flexible practices."}
-    ]
-var idx = 0
+]
+
+// Var responsável por guardar o score de cada padrão
+var orderedPatters = [
+  {"id": 0, "score": 0},
+  {"id": 1, "score": 0},
+  {"id": 2, "score": 0},
+  {"id": 3, "score": 0},
+  {"id": 4, "score": 0},
+  {"id": 5, "score": 0},
+  {"id": 6, "score": 0},
+  {"id": 7, "score": 0},
+  {"id": 8, "score": 0},
+  {"id": 9, "score": 0},
+  {"id": 10, "score": 0},
+  {"id": 11, "score": 0},
+  {"id": 12, "score": 0},
+  {"id": 13, "score": 0}
+]
 
 
+// variável com a lib de pesquisa
+let miniSearch = new MiniSearch({
+  fields: ['title', 'description'], // fields to index for full-text search
+  storeFields: ['id'] // fields to return with search results
+})
+miniSearch.addAll(patterns);
 
-function addPattern() {
+// Adiciona um elemento a lista  de padrões
+function addPattern(idx) {
   let patternList = document.getElementById("patternList");
 
   const item = document.createElement("li");
   const text = document.createTextNode(patterns[idx]["title"]);
 
   item.appendChild(text);
-  idx = idx+1;
-
   patternList.appendChild(item);
 }
 
-function Delete(currentEl){
-  currentEl.parentNode.parentNode.removeChild(currentEl.parentNode);
+/* 
+Reorganiza a lista de padrões, tanto para adição de uma nova palavra quanto remoção
+
+  type == 'INC' => Adiciona uma nova palavra buscando ela e incrementado o score dos padrões
+  type == 'DEC' => Remove uma palavra buscando ela e decrementando o score dos padrões
+
+*/
+function redefinePatternsOrder(keyword, type) {
+  // buscar palavras 
+  results = miniSearch.search(keyword);
+  console.log(results);
+
+
+  // atualizar score dos padrões
+  results.forEach(function(serchResult) {
+    const inputiId = serchResult.id;
+    const inputScore = serchResult.score;
+
+    orderedPatters.forEach(function({ id }, idx){
+      if (id === inputiId && type == 'INC') {
+        orderedPatters[idx].score += inputScore
+      } 
+    
+      if (id === inputiId && type == 'DEC') {
+        orderedPatters[idx].score -= inputScore
+      } 
+    });
+  })
+  
+
+  // reordenar lista de scores
+  orderedPatters.sort((a, b) => {
+    return b.score - a.score;
+  });
+  console.log(orderedPatters);
+  
+  // apagar lista atual
+  document.getElementById("patternList").innerHTML = "";
+
+  // escrever lista reodenada
+  orderedPatters.forEach(function(pattern) {
+    if (pattern.score > 0) {
+      addPattern(pattern.id)
+    }
+  });
+
 }
 
-
-// configurando eventos
+// Função responsável por lidar com a adição de uma palavras a busca
 function addWord() {
 
-  // Get elements
+  // Pega palavra buscada
   const field = document.getElementById('searchBar')
   const keyWord = field.value;
-  let wordsList = document.getElementById("selectedWordsDiv");
 
-  wordsList.innerHTML += '<li> <a class="tile">' + keyWord + 
-    '<button onclick="Delete(this);">X</button> <\a> </li>';
+  // Adiciona palavra a lista de palavras escolhidas
+  let wordsList = document.getElementById("selectedWordsList");
+  wordsList.innerHTML += '<li> <a class="tile">' + keyWord + '<button onclick="deleteWord(this);">X</button> <\a> </li>';
       
-  // creating item 
-  // const itemList = document.createElement("li");
-  // let aItem = document.createElement("a")
-  // aItem.classList.add("tile");
-  // const text = document.createTextNode(keyWord);
+  // Reordenar padrões
+  redefinePatternsOrder(keyWord, 'INC');
 
-  // // inserting in list
-  // aItem.appendChild(text);
-  // itemList.appendChild(aItem);
-  // wordsList.appendChild(itemList);
-
-
-  // Zerando campo
+  // Zera o campo da busca
   field.value = '';
 }
 
+// Função responsável por lidar com a remoção de uma palavras a busca
+function deleteWord(currentEl){
+  // Captura o valor da palavra removida
+  currentEl.innerHTML = ''
+  const keyWord = currentEl.parentNode.textContent
+
+  // Remove palavra da lista
+  currentEl.parentNode.parentNode.removeChild(currentEl.parentNode);
+
+  // atualiza padrões
+  redefinePatternsOrder(keyWord, 'DEC');
+}
+
+// Configurando barra de pesquisa para disparar função com Enter
 document.getElementById("searchBar")
   .addEventListener("keyup", function(e) {
     if (e.code === 'Enter') { addWord()}
